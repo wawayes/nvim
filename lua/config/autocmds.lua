1ce188c8f70aedeb2e343618280cfbdb4a08ec7d
walker <wangmingyaoyes@gmail.com>
2025-09-26 20:24:30 +0800

    Disable format-on-save for ts/tsx/js/jsx; prevent Conform/LSP formatting on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable any format-on-save for specific filetypes
+-- This prevents Conform/LSP or other formatters from running on save
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("no_format_ts_js", { clear = true }),
+  pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+  desc = "Disable format-on-save for ts/tsx/js/jsx",
+})

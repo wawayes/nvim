3b5d1f882518485cc66d58f14bf2313af0a19477
wawayes <wangmingyaoyes@gmail.com>
2025-10-10 16:44:06 +0800

    chore: update nvim configuration

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 300,
+        virt_text = true,
+        virt_text_pos = "eol",
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
+    },
+  },
+}
